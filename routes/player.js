3f5f797978e2ee2131fcd54b9073b4feceeeb51e
Manga <m.dlamini@alustudent.com>
2024-07-26 09:32:10 +0400

    player removal from team

--- routes/player.js
+++ routes/player.js
@@ -27,17 +27,27 @@ router.get('/', async (req, res) => {
 
   router.get('/delete/:id', ensureAdmin, async (req, res) => {
     try {
-      const player = await Player.findByIdAndDelete(req.params.id);
+      const playerID = req.params.id;
+      const player = await Player.findByIdAndDelete(playerID);
   
       if (!player) {
-        return res.status(404).send();
+        return res.status(404).send('Player not found');
       }
   
-      res.redirect(req.get('referer'))
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
+      // Check if the player has a team associated
+      if (player.team) {
+        await Team.updateOne(
+          { _id: player.team },
+          { $pull: { players:playerID } }
+        );
+        console.log('Player removed successfully from team');
+      }
   
+      res.redirect(req.get('referer'));
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send(error.message);
+    }
+  });  
   
   module.exports = router;
